72947c35b7b76137ab0fe5093509bc5eeeb46a63
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2015-03-16 15:57:24 +0900

    make some methods private

--- mapletree/driver.py
+++ mapletree/driver.py
@@ -9,7 +9,7 @@ from threading import Thread
 
 
 class Driver(object):
-    STUB = '--mapletree-driver-stub'
+    _STUB = '--mapletree-driver-stub'
 
     def __init__(self, app, port, target, interval):
         self.app = app
@@ -24,7 +24,7 @@ class Driver(object):
         t.start()
 
     def run(self):
-        if self.STUB in sys.argv:
+        if self._STUB in sys.argv:
             self._run_as_stub()
 
         else:
@@ -32,7 +32,7 @@ class Driver(object):
 
     def _run_as_driver(self):
         print(': starting driver')
-        stub_cmd = [sys.executable] + sys.argv + [self.STUB]
+        stub_cmd = [sys.executable] + sys.argv + [self._STUB]
         while True:
             try:
                 if subprocess.call(stub_cmd) != 0:
@@ -47,20 +47,20 @@ class Driver(object):
         self.httpd = make_server('localhost', self.port, self.app)
 
         if filewatch:
-            filewatch_thread = Thread(target=self.watch_files)
+            filewatch_thread = Thread(target=self._watch_files)
             filewatch_thread.daemon = True
             filewatch_thread.start()
 
         self.httpd.serve_forever()
 
-    def watch_files(self):
+    def _watch_files(self):
         keep_watching = True
         watch_start = time.time()
 
         while keep_watching:
             time.sleep(self.interval)
 
-            for f in self.target_files():
+            for f in self._target_files():
                 if watch_start < os.stat(f).st_mtime:
                     keep_watching = False
                     break
@@ -68,7 +68,7 @@ class Driver(object):
         print(': detected changes')
         self.httpd.shutdown()
 
-    def target_files(self):
+    def _target_files(self):
         for root, dirs, files in os.walk(self.target):
             for f in files:
                 yield os.path.join(root, f)
